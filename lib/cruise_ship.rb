a5df8d284a3f2aaea67d16d803594f76ec81f264
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-17 12:52:40 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,9 +18,9 @@ winner
 end
 
 
-# def happy_birthday(birthday_kids)
-#   birthday_kids.each do |kids_name, age|
-#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-#   end
-# end
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
