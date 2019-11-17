a1cb667fd0be320eb056a2fc06ae2b4fc28a7ecc
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-17 13:28:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,11 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids) 
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name,age|
     if age <= 12
       "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else 
       "You are too old for this."
   end
 end
